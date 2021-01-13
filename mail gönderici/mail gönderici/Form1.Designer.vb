69dc64f0dfb498becd6fcccb506e00f3cfe48e80
YodasProjects <76970992+YodasProjects@users.noreply.github.com>
2021-01-13 11:36:56 +0300

    Add files via upload

---
+++ mail gönderici/mail gönderici/Form1.Designer.vb
@@ -0,0 +1,294 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form, bileşen listesini temizlemeyi bırakmayı geçersiz kılar.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form Tasarımcısı tarafından gerektirilir
+    Private components As System.ComponentModel.IContainer
+
+    'NOT: Aşağıdaki yordam Windows Form Tasarımcısı için gereklidir
+    'Windows Form Tasarımcısı kullanılarak değiştirilebilir.  
+    'Kod düzenleyicisini kullanarak değiştirmeyin.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtsifre = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txteposta = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtport = New System.Windows.Forms.TextBox()
+        Me.txtsmtp = New System.Windows.Forms.TextBox()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtbody = New System.Windows.Forms.RichTextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txttopic = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtreciever = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(0, 0)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(335, 466)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.BackColor = System.Drawing.Color.OrangeRed
+        Me.TabPage1.Controls.Add(Me.GroupBox1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(327, 440)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Settings"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.txtsifre)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txteposta)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtport)
+        Me.GroupBox1.Controls.Add(Me.txtsmtp)
+        Me.GroupBox1.Location = New System.Drawing.Point(42, 28)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(239, 362)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Settings"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Yu Gothic UI Semibold", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label4.Location = New System.Drawing.Point(6, 171)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(80, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Mail Password"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Yu Gothic UI Semibold", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label3.Location = New System.Drawing.Point(6, 126)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(66, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Mail Adress"
+        '
+        'txtsifre
+        '
+        Me.txtsifre.Location = New System.Drawing.Point(9, 187)
+        Me.txtsifre.Name = "txtsifre"
+        Me.txtsifre.Size = New System.Drawing.Size(224, 20)
+        Me.txtsifre.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Yu Gothic UI Semibold", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label1.Location = New System.Drawing.Point(6, 72)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(59, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "SMTP Port"
+        '
+        'txteposta
+        '
+        Me.txteposta.Location = New System.Drawing.Point(9, 142)
+        Me.txteposta.Name = "txteposta"
+        Me.txteposta.Size = New System.Drawing.Size(224, 20)
+        Me.txteposta.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Yu Gothic UI Semibold", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label2.Location = New System.Drawing.Point(6, 23)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "SMTP Adress"
+        '
+        'txtport
+        '
+        Me.txtport.Location = New System.Drawing.Point(9, 88)
+        Me.txtport.Name = "txtport"
+        Me.txtport.Size = New System.Drawing.Size(75, 20)
+        Me.txtport.TabIndex = 1
+        '
+        'txtsmtp
+        '
+        Me.txtsmtp.Location = New System.Drawing.Point(6, 39)
+        Me.txtsmtp.Name = "txtsmtp"
+        Me.txtsmtp.Size = New System.Drawing.Size(233, 20)
+        Me.txtsmtp.TabIndex = 0
+        '
+        'TabPage2
+        '
+        Me.TabPage2.BackColor = System.Drawing.Color.OrangeRed
+        Me.TabPage2.Controls.Add(Me.GroupBox2)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(327, 440)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Send Mail"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button1)
+        Me.GroupBox2.Controls.Add(Me.txtbody)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.txttopic)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.txtreciever)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Location = New System.Drawing.Point(29, 19)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(266, 385)
+        Me.GroupBox2.TabIndex = 0
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Send Mail"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(81, 336)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(99, 31)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "Send Mail"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtbody
+        '
+        Me.txtbody.Location = New System.Drawing.Point(10, 171)
+        Me.txtbody.Name = "txtbody"
+        Me.txtbody.Size = New System.Drawing.Size(250, 113)
+        Me.txtbody.TabIndex = 11
+        Me.txtbody.Text = ""
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Yu Gothic UI Semibold", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label7.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label7.Location = New System.Drawing.Point(6, 148)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(41, 19)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "Mail "
+        '
+        'txttopic
+        '
+        Me.txttopic.Location = New System.Drawing.Point(6, 113)
+        Me.txttopic.Name = "txttopic"
+        Me.txttopic.Size = New System.Drawing.Size(254, 20)
+        Me.txttopic.TabIndex = 9
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Yu Gothic UI Semibold", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label6.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label6.Location = New System.Drawing.Point(6, 91)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(43, 19)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Topic"
+        '
+        'txtreciever
+        '
+        Me.txtreciever.Location = New System.Drawing.Point(6, 48)
+        Me.txtreciever.Name = "txtreciever"
+        Me.txtreciever.Size = New System.Drawing.Size(254, 20)
+        Me.txtreciever.TabIndex = 7
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Yu Gothic UI Semibold", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label5.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label5.Location = New System.Drawing.Point(6, 26)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(62, 19)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Receiver"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.SeaShell
+        Me.ClientSize = New System.Drawing.Size(331, 454)
+        Me.Controls.Add(Me.TabControl1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.Text = "Mail Sender By Yoda"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents txtsifre As TextBox
+    Friend WithEvents txteposta As TextBox
+    Friend WithEvents txtport As TextBox
+    Friend WithEvents txtsmtp As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents txtbody As RichTextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txttopic As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtreciever As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ mail gönderici/mail gönderici/Form1.vb
@@ -0,0 +1,38 @@
+﻿
+Imports System.IO
+Imports System.Net.Mail
+Imports System.Net
+Imports Microsoft.Win32
+Imports System.Security.Cryptography
+Imports System.Text
+Imports System.Management
+Imports System.CodeDom.Compiler
+Imports System.Runtime.InteropServices
+Imports System.Security
+
+Public Class Form1
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim Smtp_Server As New SmtpClient
+            Dim e_mail As New MailMessage
+            Smtp_Server.UseDefaultCredentials = False
+            Smtp_Server.Credentials = New Net.NetworkCredential(txteposta.Text, txtsifre.Text)
+            Smtp_Server.Port = txtport.Text
+            Smtp_Server.EnableSsl = True
+            Smtp_Server.Host = txtsmtp.Text
+            e_mail = New MailMessage()
+            e_mail.From = New MailAddress(txteposta.Text)
+            e_mail.To.Add(txtreciever.Text)
+            e_mail.Subject = txttopic.Text
+            e_mail.IsBodyHtml = False
+            e_mail.Body = txtbody.Text
+            Smtp_Server.Send(e_mail)
+            MsgBox("Mail Succesfully Sended")
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Class
